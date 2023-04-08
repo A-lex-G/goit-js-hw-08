db8f1193b574a8df2fdccea6cdae61429050586b
A-lex-G <118674581+A-lex-G@users.noreply.github.com>
2023-04-08 04:33:46 +0000

    Deploying to gh-pages from @ A-lex-G/goit-js-hw-08@26dd3b2744f992b5a9ca608de965df5a39074b2a 🚀

--- 03-feedback.f05e4dfd.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("submit",(function(e){e.preventDefault(),console.dir(e.currentTarget.elements);var n=new FormData(e.currentTarget);console.log(n),n.forEach((function(e,n){console.log(n,e)}))}));
-//# sourceMappingURL=03-feedback.f05e4dfd.js.map
